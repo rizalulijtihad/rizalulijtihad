e7bfcd7d977c3c49d5ce36e30ea8c3d2296b8df3
rizalulijtihad <146051176+rizalulijtihad@users.noreply.github.com>
2023-11-28 12:41:57 +0800

    Add files via upload

---
+++ 03-tipe data.java
@@ -0,0 +1,80 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // tipe data  di java
+        // integer ,byte, short, long, double, float, char, boolean
+
+        //integer (satuan)
+        int i=10;
+        System.out.println(=====INTEGRER=====);
+        System.out.println(nilai integer i = " + i );
+        Sytem.out.println("Nilai MAX : " + INTEGER.MAX_VALUE) ;
+        System.out.println ("N 03/10 PRINTLN(String x)) : vold MIN_VALUE);
+        System.out.println("Besar") ;
+        
+        //byte
+        byte b= 10;
+         System.out.println(=====BYTE=====);
+        System.out.println(nilai byte B= " + b );
+        Sytem.out.println("Nilai MAX : " + BYTE.MAX_VALUE) ;
+        System.out.println ("N 03/10 PRINTLN(String x)) : vold MIN_VALUE);
+        System.out.println("Besar") ;
+
+        // Short 
+        short s=10;
+       System.out.println(=====SHORT=====);
+        System.out.println(nilai short s = " + = s );
+        Sytem.out.println("Nilai MAX :float " + SHORT.MAX_VALUE) ;
+        System.out.println ("N 03/10 PRINTLN(String x)) : vold MIN_VALUE);
+        System.out.println("Besar") ;
+
+        // LONG
+        Long l=10;
+         System.out.println(=====LONG=====);
+        System.out.println(nilai long l = " + l );
+        Sytem.out.println("Nilai MAX : " + LONG.MAX_VALUE) ;
+        System.out.println ("N 03/10 PRINTLN(String x)) : vold MIN_VALUE);
+        System.out.println("Besar") ;
+
+        //double
+        double d=10;
+         System.out.println(=====DOUBLE=====);
+        System.out.println(nilai double d = " + d );
+        Sytem.out.println("Nilai MAX : " + DOUBLE.MAX_VALUE) ;
+        System.out.println ("N 03/10 PRINTLN(String x)) : vold MIN_VALUE);
+        System.out.println("Besar") ;
+
+        //float
+        float f=10;
+         System.out.println(=====FLOAT=====);
+        System.out.println(nilai float f = " + f );
+        Sytem.out.println("Nilai MAX : " + FLOAT.MAX_VALUE) ;
+        System.out.println ("N 03/10 PRINTLN(String x)) : vold MIN_VALUE);
+        System.out.println("Besar") ;
+
+        //char
+        char c=10;
+         System.out.println(=====CHAR=====);
+        System.out.println(nilai char c = " + c );
+        Sytem.out.println("Nilai MAX : " + CHAR.MAX_VALUE) ;
+        System.out.println ("N 03/10 PRINTLN(String x)) : vold MIN_VALUE);
+        System.out.println("Besar") ;
+        //boolean
+        boolean b=10;
+         System.out.println(=====BOOLEAN=====);
+        System.out.println(nilai boolean b = " + b );
+        Sytem.out.println("Nilai MAX : " + BOOLEAN.MAX_VALUE) ;
+        System.out.println ("N 03/10 PRINTLN(String x)) : vold MIN_VALUE);
+        System.out.println("Besar") ;
+
+
+
+
+
+
+
+
+
+
+
+   
